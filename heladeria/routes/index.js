94d8e849777fc7df75ea1d7fc67be42607e4c5a9
valeAlfaro5 <valeriasalfaro@gmail.com>
2025-08-19 17:10:04 -0600

    Creando endpoints

--- heladeria/routes/index.js
+++ heladeria/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+
+require('dotenv').config()
+console.log(process.env) 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Valeria' });
+  res.render('index', { title: 'Valeria lol' });
 });
+
+app.get('/', (req, res) => {
+  res.send('Necesito get info!')
+})
+
+app.post('/', (req, res) => {
+  res.send('Probando que tenga un endpoint')
+})
+
+
 
 module.exports = router;
